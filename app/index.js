04739b77556e1e82b6f574e03082b11e6c7fd47d
Evan-Macbook-pro <bboydart91@gmail.com>
2017-05-04 14:03:09 +0900

    webpack is added and bower is removed

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import './index.scss';
+import { a } from './scripts/modules/test';
+
+import { Vector2 } from 'three';
+
+console.log(a, Vector2);
--- app/scripts/main.js
+++
@@ -1,9 +0,0 @@
-console.log('\'Allo \'Allo!');
-console.log(2**2);
-
-
-var a = [1,2,3];
-
-a.forEach(v => {
-    console.log(v);
-});
